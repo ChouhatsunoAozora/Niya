af8d3c169da3dd89b773f751e4216c889ecce02b
ChouhatsunoAozora <6611110142@mut.ac.th>
2025-07-07 10:21:20 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("tweetButton").addEventListener("click", tweetMessage);
+});
+
+function tweetMessage() {
+    const message = "Chouhatsu no Aozora Niya\n#NiyaBNK48\n#คำท้าทายที่ปลายฟ้าConceptDay1";
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
+    window.open(tweetUrl, '_blank');
+}
